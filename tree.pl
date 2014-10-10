00f959c6c06fddd53c8b904a35d80708f20a3bb2
Kaio Cesar <programador.kaio@gmail.com>
2014-10-10 00:53:05 -0300

    1# bug list

--- tree.pl
+++ tree.pl
@@ -1,9 +1,22 @@
 #!/usr/bin/perl -w
 
 use File::Find;
+use Cwd;
+my $dir = getcwd;
+use Cwd 'abs_path';
+
+my $abs_path = abs_path(__file__);
+
+print $tabs_path;
 
 sub Listener {
-	-1 && -e && print "Folder/File link: $File::Find::name\n";
+    #-1 && -e && print "Folder/File link: $File::Find::name\n";
+    if (-d $File::Find::name) {
+        -1 && -e print "Folder: $File::Find::name\n";
+    }
+    if (-f $File::Find::name) {
+        -1 && -e print "File: $File::Find::name\n";
+    }
 }
 
 find(\&Listener, './dir');
