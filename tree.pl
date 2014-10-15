a8818dd1e72e76538106a0d68bf6a9625616c565
Kaio Cesar <programador.kaio@gmail.com>
2014-10-15 19:43:21 -0300

    Listagem

--- tree.pl
+++ tree.pl
@@ -1,35 +1,64 @@
-#!/usr/bin/perl -w
+#!usr/bin/perl -w
 
 use File::Find;
 use Cwd;
+
 my $dir = getcwd();
+my $activeDir = "";
+my $count = 0;
+my $countFounds = 0;
+my $texto = "";
+
 
 sub ReadFile{
-        my $file = @_[0];
+    my $file = $_[0];
+    my $flag = 0;
 
-        open(INFO, $file) or die $!;
-        @lines = <INFO>;
-        close INFO;
 
-        print "Size: ". scalar @lines . "\n";
+    open(INFO, $file) or die $!;
+    @lines = <INFO>;
+    close INFO;
+    
+    if ( scalar @lines > 0 ) {
+        # print "@lines \n";  
+        foreach $linhas(@lines) {
+            # $cont++ while $linhas=~ /<dest>/gi;
+            my $tag = trim($linhas);
 
-        if ( scalar @lines > 0) {
-            print "Read ok.";
+            if ($tag eq "<dest>") {
+                $flag = 1;
+                $countFounds = $countFounds + 1;
+            }
+
+            if ($flag eq 1) {
+                # print"$tag \n";
+                $texto .= "$tag \n";
+            }
+
+            if ($tag eq "</dest>") {
+                $flag = 0;
+            }
+
         }
+    }
 
 }
+
+sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
 
 sub Listener {
-    if (-d $File::Find::name) {
-        -1 && -e print "Folder: $File::Find::name\n";
-    }
-    if (-f $File::Find::name) {
-        -1 && -e print "Folder: $File::Find::name\n";
+
+    if (-f $File::Find::name && $File::Find::name =~ /estabelecimento_\d+\/anexos\/moddoc103_nfe\/\d+\/\d+\/.+\.xml/) {
         ReadFile($File::Find::name);
+        $count = $count + 1;
     }
+
 }
 
 
-find(\&Listener, "$dir/dir");
+find(\&Listener, "$dir/user_files");
 
+# print "Countdow $count found $countFounds \n";
+
+print "$texto\n";
 
